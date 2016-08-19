d4476f8b7ba6bd1de5f77ef9b14e50245635d1fc
Nils Ole Timm <Nils@playotherworld.com>
2016-08-19 10:32:06 +0200

    qemu script changes

--- qemu-test.sh
+++ qemu-test.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 QEMU_PATH=$(printf "%q" "/cygdrive/c/Program Files/qemu/qemu-system-arm")
-if [[ $(uname) = "Linux" ]]
+if [\"$(uname)\" == \"Linux\"]
 	then
 		qemu-system-arm -D qemu.log -kernel nos.elf -cpu arm1176 -m 256 -M raspi2 -d int -nographic
 else
