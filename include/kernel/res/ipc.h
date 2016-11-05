880d744d8b4e8f18363b1bb7c99254d6f3722b78
Nils Ole Timm <nils.ole.timm@googlemail.com>
2016-11-05 23:12:48 +0100

    Added kernel side ipc port table

---
+++ include/kernel/res/ipc.h
@@ -0,0 +1,19 @@
+#ifndef __K_IPC_H__
+#define __K_IPC_H__
+#define IPC_PORTS_MAX 4096
+#include <stdint.h>
+#include <sys/types.h>
+typedef struct{
+    uint32_t    owner_tid;
+    p_addr_t      loc;
+} k_ipc_port_t;
+
+typedef struct{
+    k_ipc_port_t ports[IPC_PORTS_MAX];
+} k_ipc_port_tbl_t;
+
+void kipc_init();
+int kipc_register_port(uint32_t tid, p_addr_t addr, uint32_t port);
+int kipc_unregister_port(uint32_t port);
+int kipc_map(v_addr_t target_addr, uint32_t port);
+#endif
