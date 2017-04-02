eeb9f3d6849838a728cf9e6a75af3d3f1c9bae43
Nils Ole Timm <nils.ole.timm@googlemail.com>
2017-04-02 15:20:09 +0200

    Made pi-b build script auto build hardware map

--- pi-b_build_setup.sh
+++ pi-b_build_setup.sh
@@ -6,5 +6,9 @@ cd usr/init
 make -B all PLATFORM=bcm2385
 make install
 cd ../..
+cd tools/devmap_bld
+./devmap_bld devmap_pi.txt devmap_pi-b
+cp devmap_pi-b ../../hw_map.map
+cd ../..
 make -B pi-b-all
 make install
